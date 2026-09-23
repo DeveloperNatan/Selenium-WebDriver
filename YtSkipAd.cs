9b4f6e0c0114b673987eb9106c09e4ac023240df
DeveloperNatan <natandesouza403@gmail.com>
2026-09-23 14:03:17 -0300

    feat: fix class and ajust code for run yt

--- YtSkipAd.cs
+++ YtSkipAd.cs
@@ -1,9 +1,28 @@
-namespace ConsoleApp1;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
-public static class YtSkipAd
+namespace ConsoleApp1
 {
-    public static void Run()
+
+    public static class YtSkipAd
     {
-        
+        public static void Run()
+        {
+            Console.WriteLine("Starting application...");
+            const string chromeBinary = "/var/lib/flatpak/app/com.google.Chrome/current/active/files/extra/chrome";
+            string profileDir = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".config", "selenium-chrome-profile");
+
+            var options = new ChromeOptions { BinaryLocation = chromeBinary };
+            options.AddArgument($"--user-data-dir={profileDir}");
+            options.AddArgument("--profile-directory=Default");
+            options.AddArgument("--disable-blink-features=AutomationControlled");
+            options.AddExcludedArgument("enable-automation");
+
+            IWebDriver driver = new ChromeDriver(options);
+            Console.WriteLine("ChromeDriver started.");
+
+        }
     }
 }
